8c52aa124e93835f18958f979a08d7320f098626
SerdincSelenay <157026103+SerdincSelenay@users.noreply.github.com>
2024-01-22 18:25:37 +0300

    Create List_Comprehension.py

---
+++ Python Programming for Data Science/Homework/List_Comprehension.py
@@ -0,0 +1,7 @@
+# Görev 1: List Comprehension yapısı kullanarak car_crashes verisindeki numeric değişkenlerin isimlerini
+# büyük harfe çeviriniz ve başına NUM ekleyiniz.
+
+import seaborn as sns
+df = sns.load_dataset("car_crashes")
+df.columns
+
